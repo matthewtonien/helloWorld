f549fb283009ac33a3cd29bb75188196ec0310b2
matth <matth@LAPTOP-NIMKCTQC>
2021-07-11 01:44:03 +1000

    initial commit

---
+++ JHelloWorld/src/Hello.java
@@ -0,0 +1,15 @@
+
+/**
+ * @author matthew tonien
+ * 
+ * Simple hello world program
+ */
+public class Hello {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		
+	}
+
+}
